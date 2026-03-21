52a903af96f027eb0ef27dab74fa717e94e3d7d3
Gaalo <gposada24@alumnos.uaq.mx>
2026-03-20 20:24:07 -0600

    add: client and socket working

--- C/sockets.cpp
+++ C/sockets.cpp
@@ -9,54 +9,52 @@
 // Para el uso de sockets
 #include <winsock2.h>
 #pragma comment(lib, "ws2_32.lib")
-// #include <arpa/inet.h>
-#include <unistd.h>
 
 using namespace std;
 
 int main()
 {
-    int server = 0, client = 0;
+    // INICIALIZAR WINSOCK 
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    int server, client;
     struct sockaddr_in address;
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(5050);
 
-    printf("Servidor con dirección: %d\n", address.sin_addr.s_addr);
+    printf("Servidor con direccion: %d\n", address.sin_addr.s_addr);
 
     // Socket con TPC IPv4
     server = socket(address.sin_family, SOCK_STREAM, 0);
 
     bind(server, (struct sockaddr *)&address, sizeof(address));
 
-    listen(server, 1);
+    listen(server, 5);
 
-    printf("Esperando conexión\n");
+    printf("Esperando conexion\n");
 
     while (true)
     {
         client = accept(server, NULL, NULL);
 
-        if (client == INVALID_SOCKET)
+        if (client != INVALID_SOCKET)
         {
-            // Si llega aquí sin un cliente, WSAGetLastError te dirá por qué
+            printf("Conexion recibida!\n");
+            
+            // imagen encriptada
+            vector<int> datos = {2, 3, 4, 5};
+            int datosSize = datos.size();
 
-            continue; // Reintenta esperar
+            // enviar tamaño de info
+            send(client, (const char *)&datosSize, sizeof(datosSize), 0);
+
+            // enviar info
+            send(client, (const char *)datos.data(), datosSize * sizeof(int), 0);
+
+            closesocket(client);
         }
-
-        printf("conexión recibida\n");
-
-        // imagen encriptada
-        vector<int> encoded = {2, 3, 4, 5};
-        int mesageSize = encoded.size();
-
-        // enviar tamaño de info
-        send(client, (const char *)&mesageSize, sizeof(mesageSize), 0);
-
-        // enviar info
-        send(client, (const char *)encoded.data(), mesageSize * sizeof(int), 0);
-
-        closesocket(client);
     }
     closesocket(server);
     WSACleanup();
